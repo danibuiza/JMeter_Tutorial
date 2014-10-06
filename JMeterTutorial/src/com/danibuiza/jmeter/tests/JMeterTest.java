430d04c05ee1d20ef365c7f9ce037a1e474306a0
Daniel Gutierrez Diez <danibuiza@gmail.com>
2014-10-06 17:57:14 +0200

    refactoreeed

--- JMeterTutorial/src/com/danibuiza/jmeter/tests/JMeterTest.java
+++ JMeterTutorial/src/com/danibuiza/jmeter/tests/JMeterTest.java
@@ -1,10 +1,44 @@
 package com.danibuiza.jmeter.tests;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class JMeterTest
 {
 
     public JMeterTest()
     {
-        System.out.println( "constructor" );
+        // just for testing purposes
+        System.out.println( "calling the constructor" );
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        // just for testing purposes
+        System.out.println( "calling setup" );
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        // just for testing purposes
+        System.out.println( "calling tearDown" );
+
+    }
+
+    @Test
+    public void testMe()
+    {
+        for( int i = 0; i < 30; i++ )
+        {
+            // just asserting randomly in order to check what happens in each case
+            int randomNumber = (int)( Math.random() * 100 );
+            System.out.println( randomNumber );
+            assertEquals( 0, randomNumber % 2 );
+        }
     }
 }
